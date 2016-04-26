7054efe39065e64a8bca83f62464ad7f2aff9e3e
xiamaoxialeyan <wangjian_213209@163.com>
2016-04-26 18:06:49 +0800

    modify util.js

--- util.js
+++ util.js
@@ -1,1 +1,11 @@
 var bind = function(func,context){return function(){func.apply(context,[].slice.call(arguments,0))}}
+
+var _ = {
+	each : function(){
+
+	},
+	extend : function(obj){
+		
+	}
+};
+
